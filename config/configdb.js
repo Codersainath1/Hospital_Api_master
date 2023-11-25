8c5dbd2233d485985f10db0ed33a6507e8b65ca6
Codersainath1 <142321458+Codersainath1@users.noreply.github.com>
2023-11-25 14:21:30 +0530

    Update configdb.js

--- config/configdb.js
+++ config/configdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
  async function main(){
-    await mongoose.connect('mongodb+srv://vywaharesainath:Sainath19@cluster0.zxszkkv.mongodb.net/employee_review?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://vywaharesainath:Sainath19@cluster0.zxszkkv.mongodb.net/Hospital_API_DB?retryWrites=true&w=majority');
     console.log("Connection SuccessFull !!");
 }
 main().catch((error) =>{console.log("Connection Not Success Full !!");})
